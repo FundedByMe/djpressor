f08affefd295f70925c530ae388af269e2eedf46
Beshr Kayali <beshr@fundedbyme.com>
2015-10-19 10:08:05 +0200

    Bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ install_reqs = parse_requirements('requirements.txt', session=PipSession())
 
 setup(
     name='djpressor',
-    version='0.0.6',
+    version='0.0.7',
     author='FundedByMe',
     author_email='dev@fundedbyme.com',
     maintainer='FBM',
