012a9dc0ed6855f0aa0ec95df5011fa082aca939
Beshr Kayali <beshrkayali@gmail.com>
2016-03-14 11:43:40 +0100

    Version up / Breaking changes

--- djpressor/__init__.py
+++ djpressor/__init__.py
@@ -3,4 +3,4 @@ This is a simple implementation of a fake ImageSpecField that provides
 essential functionality still used in our project but without relying on
 ImageKit.
 """
-__version__ = '0.1.5'
+__version__ = '1.0.0'
